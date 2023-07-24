1f1fd3bfebf828e6ab4b9eb36fdd7572e08e9ffd
ISTINYE\erkan.salihoglu <erkan.salihoglu@istinye.edu.tr>
2023-07-24 10:33:50 +0300

    Anasyafaya Hakkımızda action eklendi

--- Ornek6/Controllers/HomeController.cs
+++ Ornek6/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Ornek6.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
