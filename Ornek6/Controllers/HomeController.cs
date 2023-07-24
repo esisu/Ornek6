ba00d02d21d4ee12bb09acf61671bcd560f96e49
ISTINYE\erkan.salihoglu <erkan.salihoglu@istinye.edu.tr>
2023-07-24 10:35:30 +0300

    İletişim sayfası eklendi
    İletişim sayfası eklendi hom controller uzerüne

--- Ornek6/Controllers/HomeController.cs
+++ Ornek6/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Ornek6.Controllers
             return View();
         }
 
+        public IActionResult Conctact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
